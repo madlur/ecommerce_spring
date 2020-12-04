b2c5f58a93e50d74c5c2f704902627bd3442700b
Соболев Антон <2185430@gmail.com>
2020-12-04 12:23:52 +0300

    Initial commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,18 @@
+
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+
+public class Product implements Serializable {
+
+    private Long id;
+
+    private String title;
+
+    private BigDecimal cost;
+
+    public Product() {
+    }
+
+}
---
+++ src/main/java/ProductServlet.java
@@ -0,0 +1,9 @@
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet(name = "ProductServlet", urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+
+}
